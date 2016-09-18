56acf80b24c33fd721d2b863009c4be8780f5853
Marko Grdinić <mrakgr@gmail.com>
2016-09-18 11:37:31 +0200

    Added a smallish example of an alternative pipes technique.

---
+++ Futhark Parser/Scripts/pipes.fsx
@@ -0,0 +1,18 @@
+﻿/// Here is an alternative way of doing pipes. Fparsec would be better if it replaced what it did today with the following
+/// technique. The Pipes library has more elegant syntax, but in terms simplicity, the code below is hard to beat.
+
+type Stream() = class end
+
+let a (s: Stream) = 1
+let b (s: Stream) = "asd"
+let c (s: Stream) = 5.5
+
+let (<?|) (cont: Stream -> _) (fn: Stream -> _) (s: Stream) =
+    let cont = cont s
+    let fn = fn s
+    cont fn
+
+let f k = k <?| a <?| b <?| c
+let runStream f k = f <| (fun s -> k) <| Stream()
+
+let r = runStream f <| fun a b c -> b
